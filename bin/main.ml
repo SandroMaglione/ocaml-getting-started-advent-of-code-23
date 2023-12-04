7ddd069d9c09185272e0fdb3413084a4b4ef7d06
SandroMaglione <lass.maglio@gmail.com>
2023-12-04 07:14:43 +0100

    draft part 2 day 1

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,5 @@
-let () = Aoc.Day1.main
+let () =
+  print_endline "Day 1\n---------------";
+  Printf.printf "Part 1: %s\n" Aoc.Day1.main1;
+  Printf.printf "Part 2: %s\n" Aoc.Day1.main2
+;;
--- lib/day1.ml
+++ lib/day1.ml
@@ -1,5 +1,6 @@
 (* https://adventofcode.com/2023/day/1 *)
 let input_test1 = "./input/day1/test1.txt"
+let input_test2 = "./input/day1/test2.txt"
 let input_file = "./input/day1/input.txt"
 
 type digits =
@@ -26,14 +27,101 @@ let handle_line line =
   |> dig_sum
 ;;
 
-let read_and_print file_to_read =
+let read_and_print1 file_to_read =
   file_to_read
   |> In_channel.open_text
   |> In_channel.input_lines
   |> List.map handle_line
   |> List.fold_left (fun sum n -> sum + n) 0
   |> string_of_int
-  |> print_endline
 ;;
 
-let main = read_and_print input_file
+let main1 = read_and_print1 input_file
+
+(* --- *)
+
+type letters =
+  | One
+  | Two
+  | Three
+  | Four
+  | Five
+  | Six
+  | Seven
+  | Eight
+  | Nine
+
+let let_to_int ltt =
+  match ltt with
+  | One -> 1
+  | Two -> 2
+  | Three -> 3
+  | Four -> 4
+  | Five -> 5
+  | Six -> 6
+  | Seven -> 7
+  | Eight -> 8
+  | Nine -> 9
+;;
+
+let sub_option line i l =
+  try Some (String.sub line i (i + l)) with
+  | Invalid_argument _ -> None
+;;
+
+let parse_n expect res line i =
+  let l = String.length expect in
+  match sub_option line i l with
+  | Some str -> if str == expect then Some (res, i + l) else None
+  | _ -> None
+;;
+
+let parse_one = parse_n "one" One
+let parse_two = parse_n "two" Two
+let parse_three = parse_n "three" Three
+let parse_four = parse_n "four" Four
+let parse_five = parse_n "five" Five
+let parse_six = parse_n "six" Six
+let parse_seven = parse_n "seven" Seven
+let parse_eight = parse_n "eight" Eight
+let parse_nine = parse_n "nine" Nine
+
+let parse_all line i =
+  [ parse_one
+  ; parse_two
+  ; parse_three
+  ; parse_four
+  ; parse_five
+  ; parse_six
+  ; parse_seven
+  ; parse_eight
+  ; parse_nine
+  ]
+  |> List.map (fun prs -> prs line i)
+;;
+
+let rec from_char i line =
+  if String.length line >= i
+  then []
+  else (
+    let src = List.find_opt Option.is_some (parse_all line i) in
+    match Option.join src with
+    | Some (ltt, ni) -> List.cons ltt (from_char ni line)
+    | None -> from_char (i + 1) line)
+;;
+
+let rec print_list = function
+  | [] -> ""
+  | ltt :: l -> string_of_int (let_to_int ltt) ^ " " ^ print_list l
+;;
+
+let read_and_print2 file_to_read =
+  file_to_read
+  |> In_channel.open_text
+  |> In_channel.input_lines
+  |> List.map (from_char 0)
+  |> List.map print_list
+  |> List.fold_left String.cat ""
+;;
+
+let main2 = read_and_print2 input_test2
